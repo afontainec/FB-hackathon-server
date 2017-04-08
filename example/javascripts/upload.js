40f61f271553365ea4b5ac509f43844dc35a95b8
Carlos Aguirre <c.aguirre017@gmail.com>
2017-04-08 20:01:58 -0300

    LibOpus for Linux

--- example/javascripts/upload.js
+++ example/javascripts/upload.js
@@ -23,7 +23,7 @@ $('#upload-input').on('change', function () {
     }
 
     $.ajax({
-      url: 'http://localhost:3000/convert',
+      url: 'http://ec2-54-207-100-233.sa-east-1.compute.amazonaws.com:3000/convert',
       type: 'POST',
       data: formData,
       processData: false,
